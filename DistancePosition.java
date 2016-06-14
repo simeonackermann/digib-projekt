f19681e43b7fab093221335fcb13534bac2843ed
Robert Weiße <github@robert-weisse.de>
2016-06-14 14:17:31 +0200

    presentation

---
+++ DistancePosition.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Robert
+ */
+public class DistancePosition {
+    private int x, y1, y2, distance;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+    
+    public int getDistane() {
+        return distance;
+    }
+
+    public DistancePosition(int distance, int x, int y1, int y2) {
+        this.distance = distance;
+        this.x = x;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+}
